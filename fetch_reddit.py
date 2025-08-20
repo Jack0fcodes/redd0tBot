d7929d17cb41fa82fa9c921ec15743631fa5791c
Jack0fcodes <128237872+Jack0fcodes@users.noreply.github.com>
2025-08-21 06:03:03 +0800

    Update fetch_reddit.py

--- fetch_reddit.py
+++ fetch_reddit.py
@@ -23,7 +23,7 @@ LIMIT = 5
 SENT_FILE = "sent_posts.txt"
 
 CLIENT_ID = os.getenv("REDDIT_CLIENT_ID")
-CLIENT_SECRET = os.getenv("REDDIT_SECRET")
+CLIENT_SECRET = os.getenv("REDDIT_CLIENT_SECRET")
 USER_AGENT = os.getenv("REDDIT_USER_AGENT")
 TOKEN = os.getenv("TELEGRAM_TOKEN")
 CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
