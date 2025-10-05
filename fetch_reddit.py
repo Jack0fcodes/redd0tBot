d522c538c9d5c5e4eb359de7649e8964ea468c02
Jack0fcodes <128237872+Jack0fcodes@users.noreply.github.com>
2025-10-06 01:32:24 +0800

    Update fetch_reddit.py

--- fetch_reddit.py
+++ fetch_reddit.py
@@ -83,7 +83,6 @@ if __name__ == "__main__":
             "hireanartist",
             "HungryArtistsFed",
             "starvingartist",
-            "DrawForMe",
             "CatsWithDogs",
             "starvingartists"
         ]
